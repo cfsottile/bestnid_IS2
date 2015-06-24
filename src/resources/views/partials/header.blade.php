744a2835aafcf502d12a2f71db8fa1506914c839
Natii <natii@Natiidc>
2015-06-24 18:42:55 -0300

    mejora del navbar

--- src/resources/views/partials/header.blade.php
+++ src/resources/views/partials/header.blade.php
@@ -56,6 +56,7 @@
                 <li><a href="{{ route('logout') }}">Cerrar sesión</a></li>
 
 							</ul>
+            <li> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </li>
 						</li>
 					@endif
 				</ul>
